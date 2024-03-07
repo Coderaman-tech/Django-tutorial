df2c31a87db6301bbbc3d4bb08f1feeb042cc7e1
coderaman_tech <amankumarshrivastava835@gmail.com>
2024-03-07 23:46:26 +0530

    add if condition

--- telusko/travello/views.py
+++ telusko/travello/views.py
@@ -7,18 +7,21 @@ def index(request):
     dest1.desc='City of dreams'
     dest1.price=700
     dest1.img='destination_9.jpg'
+    dest1.offer=False
 
     dest2=Destination()
     dest2.name='Hyderabad'
     dest2.desc='Famous of Briyani'
     dest2.price=900
     dest2.img='destination_5.jpg'
+    dest2.offer=True
 
     dest3=Destination()
     dest3.name='Bengaluru'
     dest3.desc='IT Hub of INDIA'
     dest3.price=1000
     dest3.img='destination_4.jpg'
+    dest3.offer=True
 
     dests=[dest1,dest2,dest3]
     return render(request,"index.html",{'dests':dests})
